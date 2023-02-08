186fbcb490270d395c99ca384445c27eaf36ffa5
JohnFuhrm12 <61069716+JohnFuhrm12@users.noreply.github.com>
2023-02-07 22:42:14 -0300

    Threads and LinkedList

--- pages/destinations/[contName].tsx
+++ pages/destinations/[contName].tsx
@@ -1,5 +1,5 @@
 import { useRouter } from "next/router";
-import { useEffect } from "react";
+import Link from 'next/link';
 import styles from '@/styles/Destinations.module.css';
 import Navbar from '../../components/navbar';
 
@@ -24,10 +24,44 @@ export default function continent() {
 
     titleFromURL = titleArr.join(' ');
 
-    return (
+    // Countries Lists
+    let countries:any = [];
+    const northAmerica = ['Canada', 'United States', 'Mexico'];
+    const southAmerica = ['Colombia', 'Venezuela', 'Ecuador', 'Peru', 'Bolivia', 'Brazil', 'Chile', 'Argentina', 'Paraguay', 'Uruguay'];
+
+    switch (titleFromURL) {
+        case 'North America':
+            countries = northAmerica;
+        break;
+        case 'South America':
+            countries = southAmerica;
+        break;
+    };
+
+    return ( // <Link href="/destinations/north-america"></Link>
         <>
         <Navbar/>
         <h1 className={styles.title}>Explore The {titleFromURL} Forums</h1>
+        <div className={styles.linksList}>
+            <Link className={styles.smallLink} href="/"><h2>Home</h2></Link>
+            <h2 className={styles.linkMarker}>{'>'}</h2>
+            <Link className={styles.smallLink} href="/destinations"><h2>Destinations</h2></Link>
+            <h2 className={styles.linkMarker}>{'>'}</h2>
+            <Link className={styles.smallLink} href="#"><h2>{titleFromURL}</h2></Link>
+        </div>
+        <div className={styles.countriesContainer}>
+            <h2 className={styles.threadTitle}>Forum</h2>
+            {countries.map((country:string, index:number) => {
+                return (
+                    <>
+                    <div className={styles.forumRow}>
+                        <h2 className={styles.threadTitle} key={index}>{country}</h2>
+                        <h2 className={styles.threadTitle}>Threads: 0</h2>
+                    </div>
+                    </>
+                )
+            })}
+        </div>
         </>
     )
 }
