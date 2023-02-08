3d3933749b074e12f4c1240fa0faff85933a851c
JohnFuhrm12 <61069716+JohnFuhrm12@users.noreply.github.com>
2023-02-08 18:37:37 -0300

    Load in Threads

--- pages/destinations/[contName]/[countryName]/index.tsx
+++ pages/destinations/[contName]/[countryName]/index.tsx
@@ -5,40 +5,86 @@ import styles from '@/styles/Destinations.module.css';
 import Navbar from '../../../../components/navbar';
 import Footer from "@/components/footer";
 
+import { useEffect, useState } from "react";
+import { db } from "@/firebase";
+import { collection, doc, setDoc, deleteDoc, getDocs, query, where, limit } from "firebase/firestore";
+
 export default function country() {
     const router = useRouter();
     const {countryName} = router.query;
 
+    // State
+    const [threads, setThreads]:any = useState([]);
+    const threadsCount = threads.length;
+
+    const [continentNameURL, setContinentNameURL]:any = useState('');
+    const [continentTitleFromURL, setContinentTitleFromURL]:any = useState('');
+
+    useEffect(() => {
+        getDbmessages();
+        getContinentName();
+    }, []);
+
+    // Check For Invalid Continent Name
+    let attempts = 0;
+
+    useEffect(() => {
+        if (continentNameURL === '[contName]') {
+            getContinentName();
+        };
+        if (threadsCount === 0 && attempts < 1) {
+            getDbmessages();
+        };
+    });
+
+    // Capitalize Country Name
     let countryChars:any;
     countryChars = countryName?.toString();
-    const upperCountryName = countryChars.charAt(0).toUpperCase() + countryChars?.slice(1);
+    const upperCountryName = countryChars?.charAt(0).toUpperCase() + countryChars?.slice(1);
 
     // Get Continent Name From URL
     const { asPath } = useRouter();
-    const origin =
-        typeof window !== 'undefined' && window.location.origin
-            ? window.location.origin
-            : '';
+    let continent:any;
 
-    const URL = `${origin}${asPath}`;
-    const continent = URL.split('/')[4];
-    const titleWords = continent.split('-');
-    let continentTitleFromURL = 'destination';
-    let titleArr:any = [];
+    function getContinentName() {
+        const origin =
+            typeof window !== 'undefined' && window.location.origin
+                ? window.location.origin
+                : '';
+    
+        const URL = `${origin}${asPath}`;
+        continent = URL.split('/')[4];
+        setContinentNameURL(continent);
+        const titleWords = continent?.split('-');
+        let titleArr:any = [];
+    
+        titleWords?.forEach((word:any) => {
+            let firstChar = '';
+            const chars = word.split('');
+            firstChar = chars[0].toUpperCase();
+    
+            setContinentTitleFromURL('');
+            titleArr.push(firstChar + chars.slice(1).join(''));
+        });
+    
+        setContinentTitleFromURL(titleArr.join(' '));
+    };
 
-    titleWords?.forEach((word) => {
-        let firstChar = '';
-        const chars = word.split('');
-        firstChar = chars[0].toUpperCase();
+    // Get Country Threads
+    const threadsRef = collection(db, "threads");
 
-        continentTitleFromURL = '';
-        titleArr.push(firstChar + chars.slice(1).join(''));
-    });
+    const getDbmessages = async () => {
+        const itemsRef = query(threadsRef, where('country', '==', upperCountryName));
+        const currentQuerySnapshot = await getDocs(itemsRef);
+        setThreads(currentQuerySnapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id})));
+      };
 
-    continentTitleFromURL = titleArr.join(' ');
+      console.log(threads);
 
-    // Countries and Flags Lists
-    let countries:any = [];
+      console.log(threads.map((thread:any, index:number) => {
+        return thread.title;
+      }));
+
 
     return ( // <Link href="/destinations/north-america"></Link>
         <>
@@ -52,26 +98,14 @@ export default function country() {
             <h2 className={styles.linkMarker}>{'>'}</h2>
             <Link className={styles.smallLink} href="/destinations"><h2>Destinations</h2></Link>
             <h2 className={styles.linkMarker}>{'>'}</h2>
-            <Link className={styles.smallLink} href={`/destinations/${continent}`}><h2>{continentTitleFromURL}</h2></Link>
+            <Link className={styles.smallLink} href={`/destinations/${continentNameURL}`}><h2>{continentTitleFromURL}</h2></Link>
             <h2 className={styles.linkMarker}>{'>'}</h2>
             <Link className={styles.smallLink} href="#"><h2>{upperCountryName}</h2></Link>
         </div>
         <div className={styles.countriesContainer}>
-            <h2 className={styles.threadTitle}>Country Forums</h2>
-            {countries.map((country:any, index:number) => {
-                const sluggedCountryName = (country.countryName).toLowerCase();
-
-                return (
-                    <>
-                    <div className={styles.forumRow}>
-                        <div className={styles.titleImgContainer}>
-                            <img className={styles.flagImg} src={country.flag}/>
-                            <h2 className={styles.threadTitle} key={index}>{country.countryName}</h2>
-                        </div>
-                        <h2 className={styles.threadTitle}>Threads: 0</h2>
-                    </div>
-                    </>
-                )
+            <h2 className={styles.threadTitle}>{upperCountryName}</h2>
+            {threads.map((thread:any, index:number) => {
+                return <h2 key={index} className={styles.threadTitle}>{thread.title}</h2>
             })}
         </div>
         <Footer/>
